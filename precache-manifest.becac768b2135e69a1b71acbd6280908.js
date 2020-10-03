51f482dc68ec637e49dcc7a23df11376b3d49c87
RajibKDey <RajibKDey@users.noreply.github.com>
2020-10-03 11:58:16 +0530

    Updates

---
+++ precache-manifest.becac768b2135e69a1b71acbd6280908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f8008aacebddf23fdd903b588abd27",
+    "url": "/todo-redux-saga/index.html"
+  },
+  {
+    "revision": "064ce21f17703f746515",
+    "url": "/todo-redux-saga/static/css/main.1419fe88.chunk.css"
+  },
+  {
+    "revision": "02f8f3263e276d8c3d41",
+    "url": "/todo-redux-saga/static/js/2.b6245b92.chunk.js"
+  },
+  {
+    "revision": "aa4dbf7e9de3f8edbc6d293d3fc3907e",
+    "url": "/todo-redux-saga/static/js/2.b6245b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064ce21f17703f746515",
+    "url": "/todo-redux-saga/static/js/main.3294fe7d.chunk.js"
+  },
+  {
+    "revision": "2aa8312f25d8afebd918",
+    "url": "/todo-redux-saga/static/js/runtime-main.a5c9dc83.js"
+  }
+]);
